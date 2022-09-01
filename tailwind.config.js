d0d97c07f21551b3fc0fa9957e1c203be7decd4d
Robert Kelly <robertkelly1203@gmail.com>
2022-09-01 08:05:46 -0300

    feat: initial landing page setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      // prim == primary; sec == secondary
+      "prim-dark-blue": "#0a1929",
+      "prim-light-blue": "#66b2ff",
+      "sec-dark-blue": "#001e3c",
+      "sec-light-blue": "#5090d3",
+      "prim-white": "#ffffff",
+      "sec-white": "#757575",
+    },
     extend: {},
   },
   plugins: [],
