97dce370fd418fe52cf96fc83334707988a96962
Robert Kelly <robertkelly1203@gmail.com>
2022-10-12 09:29:52 -0300

    test: cypress basic functionalities

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,72 @@
+describe("basic functionalities", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("no empty search", () => {
+    cy.get("#search-btn").click();
+
+    cy.on("uncaught:exception", (err, runnable) => {
+      return false;
+    });
+
+    cy.get("#warning-message").should(
+      "contain.text",
+      "Search field cannot be empty"
+    );
+  });
+
+  it("no same languages search", () => {
+    cy.get("#search-box").click().type("the godfather");
+
+    cy.get("#source-lang")
+      .parent()
+      .click()
+      .get('ul > li[data-value="en-US"]')
+      .click();
+
+    cy.get("#dest-lang")
+      .parent()
+      .click()
+      .get('ul > li[data-value="en-US"]')
+      .click();
+
+    cy.get("#search-btn").click();
+
+    cy.get("#warning-message").should(
+      "contain.text",
+      "Idioms must be different"
+    );
+  });
+
+  it("search by pressing enter", () => {
+    cy.get("#search-box").click().type("the godfather{enter}");
+
+    cy.get("#movie-cards-list").should("contain.text", "The Godfather");
+  });
+
+  it("verify movie title translation", () => {
+    cy.get("#source-lang")
+      .parent()
+      .click()
+      .get('ul > li[data-value="en-US"]')
+      .click();
+
+    cy.get("#dest-lang")
+      .parent()
+      .click()
+      .get('ul > li[data-value="pt-BR"]')
+      .click();
+
+    cy.get("#search-box").click().type("the godfather{enter}");
+
+    cy.get(".MuiCardContent-root")
+      .contains("button", "See Translation")
+      .first()
+      .click();
+
+    cy.get(".MuiDialog-paper").should("contain.text", "O Poderoso Chefão");
+  });
+});
+
+export {};
